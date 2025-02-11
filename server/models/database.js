268be8604b44e6ce8a36b17f60fe4f954bc8307d
Anas khan <anas23khan083@gmail.com>
2025-02-11 19:28:17 +0530

    Update database.js

--- server/models/database.js
+++ server/models/database.js
@@ -1,16 +1,15 @@
 import mongoose from "mongoose";
 
 mongoose.connect("mongodb+srv://anas:anaskhan083@media-hub.2jmlf.mongodb.net/?retryWrites=true&w=majority&appName=media-hub")
-.then(() => console.log("Database connected"))
-.catch(() => console.log("Database connection failed"))
-
+  .then(() => console.log("Database connected"))
+  .catch(err => console.log("Database connection failed", err));
 
 const videoSchema = new mongoose.Schema({
     title: String,
     description: String,
     videoPath: String,
     thumbnailPath: String,
+    uploaderId: String,
     uploadDate: { type: Date, default: Date.now },
-})
-
-export const Video = mongoose.model("Video", videoSchema)
+});
+export const Video = mongoose.model("Video", videoSchema);
